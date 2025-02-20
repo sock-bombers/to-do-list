6c90ccda7737d4b5777cf7d6a4401d65a5c4060e
Layan Jethwa <lrsjethwa3.14@gmail.com>
2025-02-20 19:18:09 +0000

    added methods to add tasks, and checkbox (complete method not currently working)

--- project/app.py
+++ project/app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+tasks = ["task1","task2","task3"]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    new_task = request.form.get('newTask')
+    if new_task:
+        tasks.append(new_task)
+    return redirect(url_for('index'))
+
+@app.route('/complete', methods=['POST'])
+def complete_tasks():
+    completed_tasks = request.form.getlist('taskCheckbox')
+    for index in map(int, completed_tasks):
+        if 1 <= index <= len(tasks):
+            tasks[index - 1] += " - Completed"
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
