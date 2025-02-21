c6137391deb8f80fefc9e736a95a25a824b5935d
Layan Jethwa <lrsjethwa3.14@gmail.com>
2025-02-21 17:40:54 +0000

    fixed checkboxes, added README.md with to-do list (lol)

---
+++ README.md
@@ -0,0 +1,33 @@
+# To-do list project
+
+This is a to-do list project created for Hack Club's Sock event, by @shim-sham, @Kirito139, and @LayanJethwa.
+
+We have used Python Flask for the backend, and vanilla HTML for the frontend.
+## Demo
+
+You can use the site at this link: [ADD WHEN DEPLOYED]
+
+![Screenshot](LINK_TO_SCREENSHOT)
+## Features
+
+- Add tasks
+- Tick off tasks
+- Delete tasks
+## To add
+
+- Features
+    - Strikethrough on checked, move to bottom
+    - Labelling/categorisation/tagging
+    - Add date/time to tasks
+        - Allow sorting by date/time
+    - Description for tasks
+    - Sub-tasks
+- Better UI
+    - Task design
+    - Button design
+    - Checkbox design
+- Saving lists
+    - Save in-browser (cookies)
+    - Login/authentication system
+    - Importing/exporting (sharing)
+- Vercel deployment
--- project/app.py
+++ project/app.py
@@ -3,27 +3,35 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 tasks = ["task1","task2","task3"]
+tasks_checked = {"task1":0,"task2":0,"task3":0}
 
 @app.route('/')
 def index():
-    return render_template('index.html', tasks=tasks)
+    return render_template('index.html', tasks={task:tasks_checked[task] for task in tasks})
 
 @app.route('/add', methods=['POST'])
 def add_task():
     new_task = request.form.get('newTask')
     if new_task:
         tasks.append(new_task)
+        tasks_checked[new_task] = 0
     return redirect(url_for('index'))
 
 @app.route('/complete', methods=['POST'])
 def complete_tasks():
-    completed_tasks = request.form.getlist('taskCheckbox')
+    completed_tasks = request.form.getlist('checkboxId')
     for index in map(int, completed_tasks):
-        if 1 <= index <= len(tasks):
-            tasks[index - 1] += " - Completed"
+        if 0 <= index <= len(tasks):
+            if int(request.form["currentlyChecked"]):
+                tasks_checked.pop(tasks[index])
+                tasks[index] = tasks[index].replace(" - Completed","")
+                tasks_checked[tasks[index]] = 0
+            else:
+                tasks_checked.pop(tasks[index])
+                tasks[index] += " - Completed"
+                tasks_checked[tasks[index]] = 1
     return redirect(url_for('index'))
 
-# Add a new route to handle task deletion
 @app.route('/delete', methods=['POST'])
 def delete_tasks():
     task_index = request.form.get('taskIndex')
@@ -31,7 +39,7 @@ def delete_tasks():
     if task_index is not None:
         index = int(task_index)
         if 0 <= index < len(tasks):
-            del tasks[index]
+            tasks_checked.pop(tasks.pop(index))
 
     return redirect(url_for('index'))
 
