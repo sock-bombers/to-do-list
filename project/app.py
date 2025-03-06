5361b703237e168668450f3cd65f893d8d13306f
Layan Jethwa <lrsjethwa3.14@gmail.com>
2025-03-06 23:40:15 +0000

    auth now works fully and saves data, changed login/logout button, deleted indent button (can't be bothered to do logic)

--- project/app.py
+++ project/app.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+from flask_restful import Api, Resource
+from authlib.integrations.flask_client import OAuth
+from functools import wraps
 import time
 import datetime
 import json
 from os import environ as env
-from urllib.parse import quote_plus, urlencode
-
-from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
-app = Flask(__name__)
+from urllib.parse import quote_plus, urlencode
+from auth0.authentication import GetToken
+from auth0.management import Auth0
+
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
+    
+app = Flask(__name__)
 app.secret_key = env.get("APP_SECRET_KEY")
+
 oauth = OAuth(app)
+
+domain = env.get("AUTH0_DOMAIN")
 
 oauth.register(
     "auth0",
@@ -22,8 +29,13 @@ oauth.register(
     client_kwargs={
         "scope": "openid profile email",
     },
-    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+    server_metadata_url=f'https://{domain}/.well-known/openid-configuration'
 )
+
+get_token = GetToken(domain, env.get("OAUTH_CLIENT_ID"), client_secret=env.get("OAUTH_CLIENT_SECRET"))
+token = get_token.client_credentials('https://{}/api/v2/'.format(domain))
+mgmt_api_token = token['access_token']
+auth0 = Auth0(domain, mgmt_api_token)
 
 tags = {
     "Work": "#aae364",
@@ -35,16 +47,20 @@ filtered_tag = ""
 default_datetime = datetime.datetime(1971,1,1,0,0)
 default_description = "Enter description here: "
 sort = 0
+logged_in = False
 
 #  id: [task_name, completed, [tag_names], recently_added], datetime, description, description_rows}
-tasks = {
-    0: ["Task 1", 0, ["Work", "Urgent"], 0, default_datetime, "Task 1 now has a description", 1],
-    1: ["Task 2", 0, ["Personal"], 0, default_datetime, default_description, 1],
-    2: ["Task 3", 0, ["Urgent"], 0, default_datetime, default_description, 1]
-}
-
+tasks = {}
 current_index = len(tasks)
 
+def update_db():
+    if session.get('user'):
+        auth0.users.update(session.get('user')['userinfo']['sub'], {
+            'user_metadata': {
+                "tasks":json.dumps(tasks, default=str),
+                "tags":json.dumps(tags, default=str)
+            }
+        })
 
 def colour_intensity(hex):
   hex = hex.replace("#","")
@@ -55,16 +71,46 @@ def reset_recently_added(task):
   tasks[task][3] = 0
 app.jinja_env.globals.update(reset_recently_added = reset_recently_added)
 
-#log in routes
+
 @app.route("/login")
 def login():
     return oauth.auth0.authorize_redirect(
         redirect_uri=url_for("callback", _external=True)
     )
 
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    global tasks
+    global tags
+    global logged_in
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    if session.get('user'):
+        id = session.get('user')['userinfo']['sub']
+        user = auth0.users.list(search_engine='v3', q='user_id='+id)['users'][0]
+        data = user['user_metadata']
+        tasks = json.loads(data["tasks"])
+        tags = json.loads(data["tags"])
+        tasks = {int(i):tasks[i][0:4]+[datetime.datetime.strptime(tasks[i][4],'%Y-%m-%d %H:%M:%S')]+tasks[i][5:] for i in tasks}
+        logged_in = True
+
+    return redirect("/")
+
 @app.route("/logout")
 def logout():
+    global tasks
+    global current_index
+    global logged_in
+    global tags
     session.clear()
+    tasks = {}
+    current_index = len(tasks)
+    logged_in = False
+    tags = {
+    "Work": "#aae364",
+    "Personal": "#4e76d4",
+    "Urgent": "#cf0000",
+    }
     return redirect(
         "https://" + env.get("AUTH0_DOMAIN")
         + "/v2/logout?"
@@ -77,15 +123,21 @@ def logout():
         )
     )
 
-@app.route("/callback", methods=["GET", "POST"])
-def callback():
-    token = oauth.auth0.authorize_access_token()
-    session["user"] = token
-    return redirect("/")
 
-#other routes
 @app.route('/')
 def index():
+    global tasks
+    global tags
+    global logged_in
+    if session.get('user') and not logged_in:
+        id = session.get('user')['userinfo']['sub']
+        user = auth0.users.list(search_engine='v3', q='user_id='+id)['users'][0]
+        data = user['user_metadata']
+        tasks = json.loads(data["tasks"])
+        tags = json.loads(data["tags"])
+        tasks = {int(i):tasks[i][0:4]+[datetime.datetime.strptime(tasks[i][4],'%Y-%m-%d %H:%M:%S')]+tasks[i][5:] for i in tasks}
+        logged_in = True
+
     global sort
     if sort:
         if filtered_tag == "":
@@ -103,7 +155,8 @@ def index():
             unchecked_tasks = {k: v for k, v in reversed({k: v for k, v in tasks.items() if v[1] == 0 and filtered_tag in v[2]}.items())}
     tasks_arg = unchecked_tasks.copy()
     tasks_arg.update(checked_tasks)
-    return render_template('index.html', tasks=tasks_arg,tags=tags,filtered_tag=filtered_tag,sort_datetime=sort,session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+    return render_template('index.html', tasks=tasks_arg,tags=tags,filtered_tag=filtered_tag,sort_datetime=sort, session=session.get('user'))
 
 @app.route('/add', methods=['POST'])
 def add_task():
@@ -112,6 +165,9 @@ def add_task():
     if new_task:
         tasks[current_index] = [new_task, 0, [], 1, default_datetime, default_description, 1]
         current_index += 1
+
+    update_db()
+
     return redirect(url_for('index'))
 
 @app.route('/complete', methods=['POST'])
@@ -124,6 +180,8 @@ def complete_tasks():
             else:
                 tasks[index][1] = 1
             tasks[index][3] = 1
+
+    update_db()
 
     time.sleep(0.5)
 
@@ -138,6 +196,8 @@ def delete_tasks():
         if 0 <= index < current_index:
             tasks.pop(index,None)
 
+    update_db()
+
     time.sleep(0.5)
 
     return redirect(url_for('index'))
@@ -148,6 +208,8 @@ def add_tag():
     task_index = int(request.form.get('taskIndex'))
     tasks[task_index][2] += [tag]
 
+    update_db()
+
     return redirect(url_for('index'))
 
 @app.route('/delete_tag', methods=['POST'])
@@ -155,6 +217,8 @@ def delete_tag():
     tag = request.form.get('tagSelected')
     task_index = int(request.form.get('taskIndex'))
     tasks[task_index][2].remove(tag)
+
+    update_db()
 
     return redirect(url_for('index'))
 
@@ -165,7 +229,7 @@ def create_tag():
     colour = request.form.get('newTagColour')
     tags[tag] = colour
 
-    print(tag, colour)
+    update_db()
 
     return redirect(url_for('index'))
 
@@ -186,6 +250,9 @@ def update_time():
     time[0] += time[1]
     time = datetime.datetime(*map(int, time[0]))
     tasks[index][4] = time
+
+    update_db()
+
     return redirect(url_for('index'))
 
 @app.route('/sort_by_datetime', methods=['POST'])
@@ -211,7 +278,9 @@ def edit_description():
         
     tasks[index][6] = rows
 
+    update_db()
+
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0", port=env.get("PORT", 3000))
+    app.run(debug=True,host="0.0.0.0", port=env.get("PORT", 5000))
